4a9a48c51e3783a588a008a3d353c20255de8183
Sergio de Diego <sergio.de.diego.ter@gmail.com>
2022-04-10 16:35:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,56 +1,35 @@
 window.onload = inicio;
+let operacion;
 function inicio() {
 
-    document.getElementById("cifrar").addEventListener("click", function () {
-        let texto = document.getElementById("mensaje").value;
-        let desplazamiento = document.getElementById("desplazamiento").value;
-        if (isNaN(desplazamiento)) {
-            alert("Debes introducir un numero en desplazamiento");
-        } else {
-            cifrar(texto, desplazamiento);
-        }
-    });
-    document.getElementById("descifrar").addEventListener("click", function () {
-        let texto = document.getElementById("mensaje").value;
-        let desplazamiento = Number(document.getElementById("desplazamiento").value);
-        desc(texto, desplazamiento);
-    });
+    let botones = document.getElementsByTagName("button");
+    for (b of botones) {
+        b.addEventListener("click", cifrar);
+    }
 }
 
 
-function cifrar(texto, desplazamiento) {
+function cifrar(evento) {
 
     let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let resultado = "";
+    let texto = document.getElementById("mensaje").value;
     texto = texto.toUpperCase().split("");
-    desplazamiento = (desplazamiento % 26 + 26) % 26;
-
-    for (let i = 0; i < texto.length; i++) {
-        if (letras.indexOf(texto[i]) != -1) {
-            let posicion = (letras.indexOf(texto[i]) + desplazamiento) % 26;
-            resultado += letras[posicion];
-        } else {
-            resultado += texto[i];
+    let operacion = evento.target.innerHTML == "cifrar" ? "cifrar" : "";
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    if (isNaN(desplazamiento) || desplazamiento %1 != 0) {
+        alert("Debes introducir un numero entero en desplazamiento");
+    } else {
+        desplazamiento = (operacion == "cifrar") ? (desplazamiento % 26 + 26) % 26 : (desplazamiento % 26 - 26) % 26;
+        for (let i = 0; i < texto.length; i++) {
+            if (letras.indexOf(texto[i]) != -1) {
+                let posicion = (operacion == "cifrar") ? (letras.indexOf(texto[i]) + desplazamiento) % 26 :
+                    (letras.indexOf(texto[i]) - desplazamiento) % 26;
+                resultado += letras[posicion];
+            } else {
+                resultado += texto[i];
+            }
         }
+        document.getElementById("descifrado").value = resultado;
     }
-    document.getElementById("descifrado").value = resultado;
 };
-
-
-function desc(texto, desplazamiento) {
-
-    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let resultado = "";
-    texto = texto.toUpperCase().split("");
-    desplazamiento = (desplazamiento % 26 - 26) % 26;
-
-    for (let i = 0; i < texto.length; i++) {
-        if (letras.indexOf(texto[i]) != -1) {
-            let posicion = (letras.indexOf(texto[i]) - desplazamiento) % 26;
-            resultado += letras[posicion];
-        } else {
-            resultado += texto[i];
-        }
-    }
-    document.getElementById("descifrado").value = resultado;
-}; 
